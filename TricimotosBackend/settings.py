97a3bd74fdb84236e5135f587aa230b98c364ee5
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2025-07-22 00:02:02 -0500

    sexto  commit

--- TricimotosBackend/settings.py
+++ TricimotosBackend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-*ss_7p^-_mw@^()b!32ggqcgk!%rdf=v5t89mlb=%u(2=dds(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.10.170', '192.168.76.78','192.168.50.1']
+ALLOWED_HOSTS = ['localhost', 'web-production-8280a.up.railway.app', '192.168.10.170', '192.168.76.78','192.168.50.1']
 
 
 # Application definition
