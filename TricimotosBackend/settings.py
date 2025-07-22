6207c5335c1df7f91df186dbdf04b7b1b89fbe79
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2025-07-22 00:57:30 -0500

    url lista  commit

--- TricimotosBackend/settings.py
+++ TricimotosBackend/settings.py
@@ -139,3 +139,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-8280a.up.railway.app']
